732c94e3e2a146d2be0887802b2b70a7999c3200
Emre Akı <aki.emre@icloud.com>
2021-10-28 17:02:48 +0300

    feat(m_matrix): add tests for matrix inversion and reduced echelon row reduction

--- algos/main.c
+++ algos/main.c
@@ -31,7 +31,29 @@ void TestAVL (void)
     E_Dump();
 }
 
-void TestMatrix (void)
+void TestMatrixInversion (void)
+{
+    double matrix[] = { 3, -1, 0,
+                        1, 3, 0,
+                        0, 0, 1 };
+
+    double* inverted = M_Invert(matrix, 3);
+    M_Dump(inverted, 3, 3);
+    double* testforidentity = M_Mult(inverted, 3, 3, matrix, 3, 3);
+    M_Dump(testforidentity, 3, 3);
+
+    double identity[] = { 1, 0, 0,
+                          0, 1, 0,
+                          0, 0, 1 };
+
+    int equals = M_Equals(testforidentity, identity, 3, 3);
+    printf("TestMatrixInversion exited with %d.\n", !equals);
+    E_Free(inverted);
+    E_Free(testforidentity);
+    E_Dump();
+}
+
+void TestMatrixRREF (void)
 {
     /* test reducing matrix to row reduced echelon form */
     double matrix[] = { 0, 1, 0,
@@ -40,22 +62,16 @@ void TestMatrix (void)
 
     double* rref = M_ToRREF(matrix, 3, 3, 3);
     M_Dump(rref, 3, 3);
-
-    /* test inverting the matrix */
-    double invertthis[] = { 3, -1, 0,
-                            1, 3, 0,
-                            0, 0, 1 };
-
-    double* inverted = M_Invert(invertthis, 3);
-    M_Dump(inverted, 3, 3);
+    if (rref) E_Free(rref);
+    E_Dump();
 }
 
 int main (int argc, const char * argv[])
 {
     E_Init(1);
     TestAVL();
-    TestMatrix();
-    E_Dump();
+    TestMatrixInversion();
+    TestMatrixRREF();
     E_Destroy();
     return 0;
 }
